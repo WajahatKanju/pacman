f096b04091960d72fe49a5baf0cecf75155a4feb
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-08-26 06:58:27 -0700

    animated pacman

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { grid } from "./drawing.js";
 
 const canvas = document.getElementsByTagName("canvas")[0];
 const context = canvas.getContext("2d");
+const pacmanArray = [];
 
 const background = (canvas, context, color = "#000000") => {
   context.save();
@@ -11,35 +12,56 @@ const background = (canvas, context, color = "#000000") => {
   context.restore();
 };
 
-const draw_pacman = (
-  x = 200,
-  y = 200,
-  radius = 150,
-  startAngle = Math.PI * 0.2,
-  endAngle = Math.PI * 1.8
-) => {
-  grid(canvas, context);
-  context.beginPath();
-  context.arc(x, y, radius,startAngle, endAngle);
-  context.lineTo(200, 200);
-  context.fillStyle = "yellow";
-  context.fill();
-};
+class Pacman {
+  constructor(
+    x = 200,
+    y = 200,
+    radius = 150,
+    startAngle = Math.PI * 0.2,
+    endAngle = Math.PI * 1.8,
+    increase_velocity = 0.01 * Math.PI
+  ) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.increase_velocity = increase_velocity;
+  }
+  draw() {
+    grid(canvas, context);
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+    context.lineTo(200, 200);
+    context.fillStyle = "yellow";
+    context.fill();
+  }
 
-const animatePacman = () => {
-	let x = 200;
-  let y = 200;
-  let radius = 150;
-  let startAngle = Math.PI * 0.2;
-  let endAngle = Math.PI * 1.8;
-	draw_pacman(x, y, radius, startAngle, endAngle);
+  animate() {
+    if (this.startAngle < 0) {
+      this.open = true;
+    }
+		if(this.startAngle > Math.PI * 0.2){
+			this.open = false;
+		}
+    if (!this.open) {
+      this.startAngle = this.startAngle - this.increase_velocity;
+      this.endAngle = this.endAngle + this.increase_velocity;
+    } else {
+      this.startAngle = this.startAngle + this.increase_velocity;
+      this.endAngle = this.endAngle - this.increase_velocity;
+    }
+
+    this.draw();
+  }
 }
 
+let pacman = new Pacman();
 const animate = () => {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	background(canvas, context);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  background(canvas, context);
   requestAnimationFrame(animate);
-	draw_pacman();
+  pacman.animate();
 };
 
 animate();
